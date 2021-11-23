b3587116c6aabe806a4efd662fabdd87b921d710
JBeckintosh <James-beck@live.com>
2021-11-23 20:41:14 +1300

    Very basic react router addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/home/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+      <div className="App">
+        <h1>Visualising sorting algorithms</h1>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/Test'>Test</Link>
+        </nav>
+      </div>
+    </BrowserRouter>
   );
 }
 
