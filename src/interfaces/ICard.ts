fc9b000c5d4f09e0a43c51f203f69150a1ef89e0
JBeckintosh <James-beck@live.com>
2021-12-25 19:11:41 +1300

    Removing unnecessary model

--- src/interfaces/ICard.ts
+++ src/interfaces/ICard.ts
@@ -1,10 +1,3 @@
-export interface IDeck {
-    success: boolean;
-    deck_id: string;
-    shuffled: boolean;
-    remaining: number;
-}
-
 export interface ICard {
     image: string;
     value: string;
